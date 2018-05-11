58d34060f81c668622af7686a96476323da8a56c
Michael Soutar <soutarm@gmail.com>
2018-05-11 13:42:24 +1000

    Initial sample

---
+++ src/App.vue
@@ -0,0 +1,123 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Vue.js SPA</h1>
+    </header>
+    <main>
+      <aside class="sidebar">
+        <router-link
+            v-for="post in posts"
+            active-class="is-active"
+            class="link"
+            :to="{ name: 'post', params: { id: post.id } }">
+          {{post.id}}. {{post.title}}
+        </router-link>
+      </aside>
+      <div class="content">
+        <router-view></router-view>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    data () {
+      return {
+        posts: [],
+        endpoint: 'https://jsonplaceholder.typicode.com/posts/',
+      }
+    },
+    created() {
+      this.getAllPosts();
+    },
+    methods: {
+      getAllPosts() {
+        axios.get(this.endpoint)
+          .then(response => {
+            this.posts = response.data;
+          })
+          .catch(error => {
+            console.log('-----error-------');
+            console.log(error);
+          })
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'effra', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #3c515b;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #fd4974;
+}
+
+  header {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    min-height: 90px;
+    text-align: center;
+  }
+  main {
+    display: flex;
+    height: calc(100vh - 90px);
+    max-width: 1200px;
+    margin-top: 90px;
+    margin-left: auto;
+    margin-right: auto;
+    overflow: hidden;
+  }
+  aside {
+    flex: 1 0 30%;
+    height: 100%;
+    overflow-y: auto;
+    width: 30%;
+    padding: 50px 30px;
+    box-sizing: border-box;
+    text-align: left;
+  }
+    .content {
+    flex: 1 1 70%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .link {
+    display: block;
+    text-decoration: none;
+    margin-bottom: 10px;
+    color: #2c3e50;
+    &--home {
+      text-transform: uppercase;
+      margin-bottom: 30px;
+    }
+    &.is-active {
+      color: #fd4974;
+    }
+  }
+</style>
---
+++ src/components/ListingCard.vue
@@ -0,0 +1,75 @@
+<template lang="html">
+  <div class="listing" v-if="listing">
+    <h1 class="listing__title">{{ listing.title }}</h1>
+    <p class="listing__body">{{ listing.body }}</p>
+    <p  class="listing__id">{{ listing.id }}</p>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+  export default {
+    props: ['id'],
+    metaInfo() {
+      return {
+        title: this.listing && this.listing.title,
+      };
+    },
+    data() {
+      return {
+        listing: null,
+        endpoint: 'https://jsonplaceholder.typicode.com/listings/',
+      }
+    },
+    methods: {
+      getListing(id) {
+        axios(this.endpoint + id)
+          .then(response => {
+            this.listing = response.data
+          })
+          .catch( error => {
+            console.log('-----error-------');
+            console.log(error)
+          })
+      }
+    },
+    
+    created() {
+      this.getlisting(this.id);
+    },
+    watch: {
+      '$route'() {
+        this.getlisting(this.id);
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .listing {
+    position: relative;
+    max-width: 500px;
+    margin: 0 auto;
+    padding: 50px 20px 70px;
+    &__title {
+      position: relative;
+      text-transform: uppercase;
+      z-index: 1;
+    }
+    &__body {
+      position: relative;
+      z-index: 1;
+    }
+    &__id {
+      position: absolute;
+      font-size: 280px;
+      bottom: -50px;
+      margin: 0;
+      color: #eeeeee;
+      right: -20px;
+      line-height: 1;
+      font-weight: 900;
+      z-index: 0;
+    }
+  }
+</style>
